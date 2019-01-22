3c13782d9e019f4e123c09b8dfbcab524a08cf49
okputadora <okputadora@gmail.com>
2019-01-21 21:51:17 -0500

    displaying crossword from scraped

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
+import './App.css';
+import crossword from './crossword.json'
+const App = () => {
+
+  let rows = Object.keys(crossword.board).map(row => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <tr className='row'>
+        {crossword.board[row].map(cell => {
+          let black = cell === '#BlackSquare#'
+
+          return <td className={black ? 'black' : 'cell'}>{black ? null : cell}</td>
+        })}
+      </tr>
+    )
+  })
+
+  return (
+    <div className="page">
+      <table className="board">
+        {rows}
+      </table>
+      <div className="clues">
+        <div className="acrossClues">
+          {crossword.clues.map(clue => {
+            return <div className="clue">{clue.clue}</div>
+          })}
+        </div>
+        <div className="downClues"></div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
